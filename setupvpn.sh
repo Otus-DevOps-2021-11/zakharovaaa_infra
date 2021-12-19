e9d0d1afc7524aa749e5861699c70f24c8545b29
zakharovaaa <96010316+zakharovaaa@users.noreply.github.com>
2021-12-19 11:30:43 +0300

    Update setupvpn.sh

--- setupvpn.sh
+++ setupvpn.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.4 multiverse" > /etc/apt/sources.list.d/mongodb-org-3.4.list
-echo "deb http://repo.pritunl.com/stable/apt xenial main" > /etc/apt/sources.list.d/pritunl.list
-apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 0C49F3730359A14518585931BC711F9BA15703C6
+echo "deb http://repo.pritunl.com/stable/apt focal main" | tee /etc/apt/sources.list.d/pritunl.list
+apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv E162F504A20CDF15827F718D4B7C549A058F8B6B
 apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 7568D9BB55FF9E5287D586017AE645C0CF8E292A
 apt-get --assume-yes update
 apt-get --assume-yes upgrade
-apt-get --assume-yes install pritunl mongodb-org
+apt-get --assume-yes install mongodb-org
+apt-get install pritunl -y
 systemctl start pritunl mongod
 systemctl enable pritunl mongod
